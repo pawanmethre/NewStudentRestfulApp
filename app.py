9a377a6f00a2fd58c1b88aa7a946c2d91108ce8f
pawanmethre <pawanmethre@gmail.com>
2020-02-06 10:48:44 +0530

    added host and port in app.run()

--- app.py
+++ app.py
@@ -4,15 +4,12 @@ import sys
 sys.path.append('/home/pawan/PycharmProjects/StudentApp')
 from entities.student_entity import Student, Students
 from  entities.user_entity import SignIn, UserRegister
-
-
-app = Flask(__name__)
 api = Api(app)
-#
 api.add_resource(Student, '/student')
 api.add_resource(UserRegister, '/auth/register')
 api.add_resource(SignIn, '/auth/signIn')
 api.add_resource(Students, '/student/students')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    PORT = int(os.environ.get("PORT", 5000))
+    app.run(debug=True, host='0.0.0.0', port=PORT)
