939f7fef31fbfea7a25638424c3704ac43a21dd1
pawanmethre <pawanmethre@gmail.com>
2020-02-06 10:53:15 +0530

    fixed app=Flask(__name__) bug

--- app.py
+++ app.py
@@ -4,6 +4,8 @@ import sys
 sys.path.append('/home/pawan/PycharmProjects/StudentApp')
 from entities.student_entity import Student, Students
 from  entities.user_entity import SignIn, UserRegister
+
+app = Flask(__name__)
 api = Api(app)
 api.add_resource(Student, '/student')
 api.add_resource(UserRegister, '/auth/register')
